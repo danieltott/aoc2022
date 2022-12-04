736fe9c97fa040310020de7afa7bfde1025f46ca
Dan Ott <dan@dtott.com>
2022-12-04 09:38:59 -0500

    Solved!

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -22,7 +22,11 @@ const part2 = (rawInput: string) => {
 
   return input.reduce((total, row) => {
     const [aStart, aStop, bStart, bStop] = row.split(/[,-]/).map(Number);
-    if (aStop <= bStart || bStop <= aStart) {
+
+    if (aStop >= bStart && aStop <= bStop) {
+      return total + 1;
+    }
+    if (bStop >= aStart && bStop <= aStop) {
       return total + 1;
     }
 
@@ -48,12 +52,20 @@ run({
   part2: {
     tests: [
       {
-        input: `2-4,6-8
+        input: `17-92,7-17
+2-4,6-8
 2-3,4-5
 5-7,7-9
 2-8,3-7
 6-6,4-6
 2-6,4-8`,
+        expected: 5,
+      },
+      {
+        input: `3-99,2-98
+23-92,23-24
+92-92,19-93
+3-94,3-96`,
         expected: 4,
       },
     ],
