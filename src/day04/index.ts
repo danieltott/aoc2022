2b493b3b23da4e3e1d270f3773369250f9e4009f
Dan Ott <dan@dtott.com>
2022-12-04 09:32:36 -0500

    part 2 - failed

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -20,7 +20,14 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  return input.reduce((total, row) => {
+    const [aStart, aStop, bStart, bStop] = row.split(/[,-]/).map(Number);
+    if (aStop <= bStart || bStop <= aStart) {
+      return total + 1;
+    }
+
+    return total;
+  }, 0);
 };
 
 run({
@@ -40,10 +47,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`,
+        expected: 4,
+      },
     ],
     solution: part2,
   },
