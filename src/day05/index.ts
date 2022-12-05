6d187517a97ae66edbf40b8c7afa5bdec3ae098e
Dan Ott <dan@dtott.com>
2022-12-05 10:10:43 -0500

    refactor to move init stuff into parseInput

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -6,48 +6,47 @@ const parseInput = (rawInput: string) => {
   if (rawInput.indexOf("\n\n") === -1 || rawInput.indexOf("\n\n") === 0) {
     throw new Error("invalid input");
   }
-  return rawInput.split("\n\n").map((group) => group.split("\n")) as [
-    string[],
-    string[],
-  ];
+  const [stacksRows, steps] = rawInput
+    .split("\n\n")
+    .map((group) => group.split("\n")) as [string[], string[]];
+
+  const labels = stacksRows[stacksRows.length - 1]
+    .split(/ (\d) /)
+    .filter((a) => !!a.trim());
+
+  const initialStacks = labels.reduce((stacksObj, label, index) => {
+    return {
+      ...stacksObj,
+      [label]: stacksRows
+        .map((row) => {
+          return row[index * 4 + 1].trim();
+        })
+        .filter(Boolean),
+    };
+  }, {} as Stacks);
+
+  return [labels, initialStacks, steps] as [string[], Stacks, string[]];
 };
 
 const part1 = (rawInput: string) => {
-  const [stacksRows, steps] = parseInput(rawInput);
+  const [labels, initialStacks, steps] = parseInput(rawInput);
 
-  const labelRow = stacksRows.pop();
-
-  if (labelRow) {
-    const labels = labelRow.split(/ (\d) /).filter((a) => !!a.trim());
-
-    const initialStacks = labels.reduce((stacksObj, label, index) => {
-      return {
-        ...stacksObj,
-        [label]: stacksRows
-          .map((row) => {
-            return row[index * 4 + 1].trim();
-          })
-          .filter(Boolean),
+  const movedStacks = steps.reduce(
+    (stacks, step) => {
+      const [countStr, from, to] = step.substring(5).split(/ from | to /);
+      const count = parseInt(countStr, 10);
+      const x = {
+        ...stacks,
+        [from]: stacks[from].slice(count),
+        [to]: [...stacks[from].slice(0, count).reverse(), ...stacks[to]],
       };
-    }, {} as Stacks);
 
-    const movedStacks = steps.reduce(
-      (stacks, step) => {
-        const [countStr, from, to] = step.substring(5).split(/ from | to /);
-        const count = parseInt(countStr, 10);
-        const x = {
-          ...stacks,
-          [from]: stacks[from].slice(count),
-          [to]: [...stacks[from].slice(0, count).reverse(), ...stacks[to]],
-        };
+      return x;
+    },
+    { ...initialStacks } as Stacks,
+  );
 
-        return x;
-      },
-      { ...initialStacks } as Stacks,
-    );
-
-    return labels.map((label) => movedStacks[label][0]).join("");
-  }
+  return labels.map((label) => movedStacks[label][0]).join("");
 };
 
 const part2 = (rawInput: string) => {
@@ -91,13 +90,34 @@ move 1 from 1 to 2`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        /**
+ * VScode is really messing up this input with its auto formatting. Here's the actual input:
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+ */
+        input:
+          ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]", " 1   2   3 "].join(
+            "\n",
+          ) +
+          `
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`,
+        expected: "MCD",
+      },
     ],
     solution: part2,
   },
   trimTestInputs: false,
-  onlyTests: false,
+  onlyTests: true,
 });
