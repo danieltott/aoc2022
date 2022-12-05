4f3b27e48c4a758d9755c589f10dfd90f91b4e6e
Dan Ott <dan@dtott.com>
2022-12-05 10:04:36 -0500

    Part 1 solved!

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,15 +1,59 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+type Stacks = Record<string, string[]>;
+
+const parseInput = (rawInput: string) => {
+  if (rawInput.indexOf("\n\n") === -1 || rawInput.indexOf("\n\n") === 0) {
+    throw new Error("invalid input");
+  }
+  return rawInput.split("\n\n").map((group) => group.split("\n")) as [
+    string[],
+    string[],
+  ];
+};
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const [stacksRows, steps] = parseInput(rawInput);
 
-  return;
+  const labelRow = stacksRows.pop();
+
+  if (labelRow) {
+    const labels = labelRow.split(/ (\d) /).filter((a) => !!a.trim());
+
+    const initialStacks = labels.reduce((stacksObj, label, index) => {
+      return {
+        ...stacksObj,
+        [label]: stacksRows
+          .map((row) => {
+            return row[index * 4 + 1].trim();
+          })
+          .filter(Boolean),
+      };
+    }, {} as Stacks);
+
+    const movedStacks = steps.reduce(
+      (stacks, step) => {
+        const [countStr, from, to] = step.substring(5).split(/ from | to /);
+        const count = parseInt(countStr, 10);
+        const x = {
+          ...stacks,
+          [from]: stacks[from].slice(count),
+          [to]: [...stacks[from].slice(0, count).reverse(), ...stacks[to]],
+        };
+
+        return x;
+      },
+      { ...initialStacks } as Stacks,
+    );
+
+    return labels.map((label) => movedStacks[label][0]).join("");
+  }
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+
+  "[Z] [M] [P]".split(/\[([A-Z])\]/);
 
   return;
 };
@@ -17,10 +61,31 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        /**
+ * VScode is really messing up this input with its auto formatting. Here's the actual input:
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+ */
+        input:
+          ["    [D]    ", "[N] [C]    ", "[Z] [M] [P]", " 1   2   3 "].join(
+            "\n",
+          ) +
+          `
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`,
+        expected: "CMZ",
+      },
     ],
     solution: part1,
   },
@@ -33,6 +98,6 @@ run({
     ],
     solution: part2,
   },
-  trimTestInputs: true,
+  trimTestInputs: false,
   onlyTests: false,
 });
