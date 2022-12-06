01ea10e1ddf2a4c7f02f13e576b6ac32b241b708
Dan Ott <dan@dtott.com>
2022-12-06 08:59:50 -0500

    Day06 (#3)
    * begin day 6!
    
    * part 1
    
    * add part 2 tests
    
    * move split to parse fn
    
    * abstract fn
    
    * solved!

---
+++ src/day06/index.ts
@@ -0,0 +1,107 @@
+import run from "aocrunner";
+
+/**
+ * -----
+ * Input parser - is used in parts 1 and 2
+ * -----
+ */
+
+const parseInput = (rawInput: string) => rawInput.split("");
+
+function findCode(input: string[], codeLength: number) {
+  let i = 0;
+  let found;
+  while (!found) {
+    if (new Set(input.slice(i, i + codeLength)).size === codeLength) {
+      found = i;
+    }
+    i++;
+  }
+
+  return found + codeLength;
+}
+
+/**
+ * -----
+ * Part 1
+ * -----
+ */
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return findCode(input, 4);
+};
+
+/**
+ * -----
+ * Part 2
+ * -----
+ */
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return findCode(input, 14);
+};
+
+/**
+ * -----
+ * Runner
+ * -----
+ */
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 7,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 5,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 6,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 10,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 11,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+        expected: 19,
+      },
+      {
+        input: `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+        expected: 23,
+      },
+      {
+        input: `nppdvjthqldpwncqszvftbrmjlhg`,
+        expected: 23,
+      },
+      {
+        input: `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`,
+        expected: 29,
+      },
+      {
+        input: `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`,
+        expected: 26,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
