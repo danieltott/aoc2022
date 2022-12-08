b4f1fd655eb632220646a105ede0e0050064b41d
Dan Ott <dan@dtott.com>
2022-12-08 01:23:32 -0500

    begin

---
+++ src/day08/index.ts
@@ -0,0 +1,62 @@
+import run from "aocrunner";
+
+/**
+ * -----
+ * Input parser - is used in parts 1 and 2
+ * -----
+ */
+
+const parseInput = (rawInput: string) => rawInput;
+
+/**
+ * -----
+ * Part 1
+ * -----
+ */
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+/**
+ * -----
+ * Part 2
+ * -----
+ */
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+/**
+ * -----
+ * Runner
+ * -----
+ */
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});
