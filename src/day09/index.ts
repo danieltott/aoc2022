b40f611f702309f672cb6c755755140595c19d28
Dan Ott <dan@dtott.com>
2022-12-09 13:57:00 -0500

    refactor into fn

--- src/day09/index.ts
+++ src/day09/index.ts
@@ -15,6 +15,58 @@ const parseInput = (rawInput: string) =>
     return [affectedColumn, move, parseInt(distance)];
   });
 
+function doTheRopeThing(
+  input: ReturnType<typeof parseInput>,
+  rope: number[][],
+) {
+  const visited = new Set([rope[rope.length - 1].join("-")]);
+
+  for (let i = 0; i < input.length; i++) {
+    const [affectedColumn, direction, distance] = input[i];
+
+    for (let j = 0; j < distance; j++) {
+      const otherColumn = affectedColumn ? 0 : 1;
+      // move the head
+      rope[0][affectedColumn] += direction;
+
+      for (let j = 1; j < rope.length; j++) {
+        const currentKnot = rope[j];
+        const previousKnot = rope[j - 1];
+
+        // if that moved it too far that direction, move the tail in the same direction
+        if (
+          Math.abs(previousKnot[affectedColumn] - currentKnot[affectedColumn]) >
+          1
+        ) {
+          currentKnot[affectedColumn] += Math.sign(
+            previousKnot[affectedColumn] - currentKnot[affectedColumn],
+          );
+
+          // if the tail is now diagonal to the head, move the tail into the same column as the head
+          if (
+            previousKnot[0] !== currentKnot[0] &&
+            previousKnot[1] !== currentKnot[1]
+          ) {
+            currentKnot[otherColumn] += Math.sign(
+              previousKnot[otherColumn] - currentKnot[otherColumn],
+            );
+          }
+        } else if (
+          Math.abs(previousKnot[otherColumn] - currentKnot[otherColumn]) > 1
+        ) {
+          // if we had previously moved to far away the last time, move the tail that direction
+          currentKnot[otherColumn] += Math.sign(
+            previousKnot[otherColumn] - currentKnot[otherColumn],
+          );
+        }
+      }
+
+      visited.add(rope[rope.length - 1].join("-"));
+    }
+  }
+  return visited;
+}
+
 /**
  * -----
  * Part 1
@@ -23,64 +75,14 @@ const parseInput = (rawInput: string) =>
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  console.log(input);
-  const headPosition = [0, 0];
-  const tailPosition = [0, 0];
-  const visited = new Set([tailPosition.join("-")]);
+  // console.log(input);
+  const rope = [
+    [0, 0],
+    [0, 0],
+  ];
 
-  for (let i = 0; i < input.length; i++) {
-    const [affectedColumn, direction, distance] = input[i];
-    console.log(input[i]);
+  const visited = doTheRopeThing(input, rope);
 
-    for (let j = 0; j < distance; j++) {
-      const otherColumn = affectedColumn ? 0 : 1;
-      // move the head
-      headPosition[affectedColumn] += direction;
-
-      // if that moved it too far that direction, move the tail in the same direction
-      if (
-        Math.abs(headPosition[affectedColumn] - tailPosition[affectedColumn]) >
-        1
-      ) {
-        tailPosition[affectedColumn] += Math.sign(
-          headPosition[affectedColumn] - tailPosition[affectedColumn],
-        );
-
-        // if the tail is now diagonal to the head, move the tail into the same column as the head
-        if (
-          headPosition[0] !== tailPosition[0] &&
-          headPosition[1] !== tailPosition[1]
-        ) {
-          tailPosition[otherColumn] += Math.sign(
-            headPosition[otherColumn] - tailPosition[otherColumn],
-          );
-        }
-      } else if (
-        Math.abs(headPosition[otherColumn] - tailPosition[otherColumn]) > 1
-      ) {
-        // if we had previously moved to far away the last time, move the tail that direction
-        tailPosition[otherColumn] += Math.sign(
-          headPosition[otherColumn] - tailPosition[otherColumn],
-        );
-      }
-
-      visited.add(tailPosition.join("-"));
-
-      console.log({
-        headPosition,
-        tailPosition,
-      });
-    }
-  }
-
-  console.log([...visited].sort());
-  /*
-..##..
-...##.
-.####.
-....#.
-s###..
-*/
   return visited.size;
 };
 
@@ -122,13 +124,33 @@ R 2`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`,
+        expected: 1,
+      },
+      {
+        input: `
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`,
+        expected: 36,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 });
