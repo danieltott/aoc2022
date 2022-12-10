6e00dbf26bb753fa601e562050a6cce5b12c676f
Dan Ott <dan@dtott.com>
2022-12-10 12:51:59 -0500

    wooooo

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -30,12 +30,6 @@ const part1 = (rawInput: string) => {
 
       if (line === "noop") {
         if (isInteresting(updated.cycle)) {
-          console.log({
-            s: 1,
-            cycle: updated.cycle,
-            x: updated.x,
-            interestingValue: updated.cycle * updated.x,
-          });
           updated.interestingSum += updated.cycle * updated.x;
         }
         return updated;
@@ -44,24 +38,12 @@ const part1 = (rawInput: string) => {
       const value = parseInt(line.slice(4));
 
       if (isInteresting(updated.cycle)) {
-        console.log({
-          s: 2,
-          cycle: updated.cycle,
-          x: updated.x,
-          interestingValue: updated.cycle * updated.x,
-        });
         updated.interestingSum += updated.cycle * updated.x;
       }
 
       updated.cycle += 1;
 
       if (isInteresting(updated.cycle)) {
-        console.log({
-          s: 3,
-          cycle: updated.cycle,
-          x: updated.x,
-          interestingValue: updated.cycle * updated.x,
-        });
         updated.interestingSum += updated.cycle * updated.x;
       }
 
@@ -83,10 +65,72 @@ const part1 = (rawInput: string) => {
  * -----
  */
 
+function getChar(position: number, x: number) {
+  let char;
+  if (position >= x && position <= x + 2) {
+    char = "#";
+  } else {
+    char = ".";
+  }
+
+  if (position === 40) {
+    return char + "\n";
+  }
+  return char;
+}
+
+function getPosition(cycle: number) {
+  const position = cycle % 40;
+  if (position === 0) {
+    return 40;
+  }
+  return position;
+}
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const result = input.reduce(
+    (acc, line) => {
+      const updated = {
+        ...acc,
+        cycle: acc.cycle + 1,
+      };
+      console.log("cycle", updated.cycle);
+
+      let position = getPosition(updated.cycle);
+
+      updated.message += getChar(position, updated.x);
+
+      if (line === "noop") {
+        return updated;
+      }
+
+      const value = parseInt(line.slice(4));
+
+      updated.cycle += 1;
+      console.log("cycle", updated.cycle);
+
+      position = getPosition(updated.cycle);
+
+      updated.message += getChar(position, updated.x);
+
+      updated.x += value;
+
+      return updated;
+    },
+    {
+      x: 1,
+      cycle: 0,
+      message: "",
+    },
+  );
+
+  console.log(result.message);
+
+  return result.message;
+
+  // RZHFGJCB
 };
 
 /**
@@ -253,10 +297,162 @@ noop`,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+        expected: `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+`,
+      },
     ],
     solution: part2,
   },
